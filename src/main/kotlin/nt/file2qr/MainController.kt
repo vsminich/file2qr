87c4b5c244f6b3d34f0fd7b96f1c2d5a1e9f8610
Vladimir Min <vsminich@gmail.com>
2022-04-23 21:44:21 +0300

    Init

--- src/main/kotlin/nt/file2qr/MainController.kt
+++ src/main/kotlin/nt/file2qr/MainController.kt
@@ -1,6 +1,7 @@
 package nt.file2qr
 
 import nt.file2qr.Base45Encoder.encodeToBase45QrPayload
+import org.apache.tomcat.util.codec.binary.Base64
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.io.BufferedInputStream
 import java.lang.Integer.min
 import java.net.URL
+import java.nio.ByteBuffer
 import javax.servlet.http.HttpServletResponse
 
 
@@ -25,7 +27,9 @@ class MainController {
     }
 
     var prepared: ByteArray = "asdf khasldjf asjkdfh asjkdfh kjashdf ".toByteArray()
+    var base64mode: Boolean = false
     val defaultLen = 5
+    val defaultImageSize = 800
 
     @GetMapping("/start")
     @ResponseBody
@@ -33,24 +37,40 @@ class MainController {
         @RequestParam(required = false) len: Int?,
         @RequestParam(required = false) off: Int?,
         @RequestParam(required = false) int: Int?,
+        @RequestParam(required = false) size: Int?,
+        @RequestParam(required = false) base64: Boolean?,
+        @RequestParam(required = false) extended: Boolean?,
     ): String {
+        base64mode = base64 ?: false
         val l = len ?: defaultLen
         val o = off ?: 0
         val int0 = int ?: 1000
-        val result = "<html><body><img src=\"/qr?len=$l&off=$o\"></img></body></html>"
+        val result = "<html><body><img src=\"/qr?len=$l&off=$o&size=${size ?: defaultImageSize}&base64=$base64mode&extended=${extended ?: true}\"></img></body></html>"
         return if (o + l >= prepared.size) result else
-            result +
-                    "<script>\n" +
-                    "         setTimeout(next, $int0);\n" +
-                    "         function next(){\n" +
-                    "            window.location.href = '/start?len=$l&off=${o + l}&int=$int0';\n" +
-                    "         }\n" +
-                    "      </script>"
+            result + """
+                <script>
+                    document.addEventListener('DOMContentLoaded', function(){
+                        setTimeout(next, $int0);
+                    });
+                    function next(){
+                        window.location.href = '/start?len=$l&off=${o + l}&int=$int0&size=${size ?: defaultImageSize}&base64=$base64mode&extended=${extended ?: true}';
+                    }
+                </script>
+            """.trimIndent()
     }
 
     @GetMapping("/prepare")
     fun prepare(@RequestParam lnk: String) {
         prepared = download(lnk)
+    }
+
+    fun ByteArray.buildBuffer(start: Int, len: Int, size: Int, extended: Boolean): ByteArray {
+        val end = min(start + len, size)
+        val data = copyOfRange(start, end)
+        return if (extended) ByteBuffer.allocate(Int.SIZE_BYTES).putInt(start).array()
+            .plus(ByteBuffer.allocate(Int.SIZE_BYTES).putInt(end).array())
+            .plus(data)
+        else data
     }
 
     @GetMapping("/qr")
@@ -59,18 +79,25 @@ class MainController {
         @RequestParam len: Int,
         @RequestParam off: Int,
         @RequestParam(required = false) size: Int?,
+        @RequestParam(required = false) base64: Boolean?,
+        @RequestParam(required = false) extended: Boolean?,
     ) {
         response.contentType = MediaType.IMAGE_JPEG_VALUE
-
-        println("$len $off")
-
         prepared
             ?.takeUnless { off > prepared.size }
-            ?.copyOfRange(off, min(off + len, prepared.size))
-            ?.let(::encodeToBase45QrPayload)
+            ?.buildBuffer(off, len, prepared.size, extended ?: true)
+            /*?.also {
+
+                println(it.size)
+                ByteBuffer.wrap(it.copyOfRange(0,4)).int.let(::println)
+                ByteBuffer.wrap(it.copyOfRange(4,8)).int.let(::println)
+            }*/
+            ?.let {
+                if (base64 == true) it.let(Base64::encodeBase64).let(::String) else it.let(::encodeToBase45QrPayload)
+            }
             ?.let {
                 QrCode.createQR(
-                    it, "png", size ?: 500, size ?: 500,
+                    it, "png", size ?: defaultImageSize, size ?: defaultImageSize,
                     response.outputStream
                 )
             }
